5ca13454a787d609c9bd2fa3efb3f0117ce3b3dd
Jianxun Luo <jxluo@jp.jxluo.me>
2019-03-17 15:12:28 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+测试一个结果，请调用：
+```
+python eval.py --input=${path_to_result_file}
+```
+打印出的浮点值就是测试结果
---
+++ eval.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-input', '--input', type=str, help='The path of input test file')
+    args = parser.parse_args()
+
+    print (0.9)
+
+
+main()
